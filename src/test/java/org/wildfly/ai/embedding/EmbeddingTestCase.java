a73b0f0150af584a43cbd295a23c18a5b27f7305
Emmanuel Hugonnet <ehugonne@redhat.com>
2024-02-16 18:43:47 +0100

    Trying retrieval chain
    Signed-off-by: Emmanuel Hugonnet <ehugonne@redhat.com>

--- src/test/java/org/wildfly/ai/embedding/EmbeddingTestCase.java
+++ src/test/java/org/wildfly/ai/embedding/EmbeddingTestCase.java
@@ -4,6 +4,7 @@
  */
 package org.wildfly.ai.embedding;
 
+import dev.langchain4j.chain.ConversationalRetrievalChain;
 import dev.langchain4j.data.message.ChatMessage;
 import dev.langchain4j.data.message.SystemMessage;
 import dev.langchain4j.data.message.UserMessage;
@@ -11,11 +12,15 @@ import dev.langchain4j.data.segment.TextSegment;
 import dev.langchain4j.model.chat.ChatLanguageModel;
 import dev.langchain4j.model.embedding.AllMiniLmL6V2EmbeddingModel;
 import dev.langchain4j.model.embedding.EmbeddingModel;
+import dev.langchain4j.model.input.PromptTemplate;
 import dev.langchain4j.model.ollama.OllamaChatModel;
+import dev.langchain4j.rag.DefaultRetrievalAugmentor;
 import dev.langchain4j.rag.content.Content;
+import dev.langchain4j.rag.content.injector.DefaultContentInjector;
 import dev.langchain4j.rag.content.retriever.ContentRetriever;
 import dev.langchain4j.rag.content.retriever.EmbeddingStoreContentRetriever;
 import dev.langchain4j.rag.query.Query;
+import dev.langchain4j.rag.query.router.DefaultQueryRouter;
 import dev.langchain4j.store.embedding.EmbeddingStore;
 import java.io.File;
 import java.time.Duration;
@@ -36,6 +41,7 @@ import org.wildfly.ai.document.parser.HtmlDocumentParser;
 public class EmbeddingTestCase {
 
     public static final String OPENAI_KEY = "demo";
+
     /**
      * Test of parsePage method, of class HtmlDocumentParser.
      */
@@ -68,18 +74,14 @@ public class EmbeddingTestCase {
         String modelName = "mistral:7b-text-q2_K";
         modelName = "mistral:7b-instruct-v0.2-q2_K";
 
-
-
-
 //        String ollamaUrl = "http://ollama-mchomaredhatcom.apps.ai-hackathon.qic7.p1.openshiftapps.com";
 //        String modelName = "7b-text-q2_Kchat";
-
         ChatLanguageModel model = OllamaChatModel.builder()
                 .baseUrl(ollamaUrl)
                 .modelName(modelName)
                 .timeout(Duration.ofSeconds(210))
                 .build();
-        
+
 //        ChatLanguageModel model = OpenAiChatModel
 //                .builder()
 //                .apiKey(OPENAI_KEY)
@@ -88,13 +90,20 @@ public class EmbeddingTestCase {
 //                .logRequests(Boolean.TRUE)
 //                .logResponses(Boolean.TRUE)
 //                .build();
-
-        String prompt = "How do I set up a ConnectionFactory to a remote jms broker ?";
-        List<Content> ragContents = contentRetriever.retrieve(Query.from(prompt));
+        String question = "How do I set up a ConnectionFactory to a remote jms broker ?";
+        List<Content> ragContents = contentRetriever.retrieve(Query.from(question));
 
         List<ChatMessage> messages = new ArrayList<>();
-        
+
         SystemMessage systemMessage = SystemMessage.systemMessage("You are a wildfly expert who understands well how to administrate the wildfly server and its components");
+        String promptTemplate = "You are a WildFly expert who understands well how to administrate the WildFly application server and its components\n"
+                + "You are also knowledgeable in the Java language.\n"
+                + "You excel at teaching and explaining concepts of the language."
+                + "If you don't know the answer to the question, say that you don't know the answer, and that the user should refer to the WildFly documentation.\n\n"
+                + "Answer the following question to the best of your ability: \n"
+                + "{{userMessage}}\n\n"
+                + "Base your answer exclusively on the following information from the WildFly documentation:\n\n"
+                + "{{contents}}";
         String completePrompt = "Objective: answer the user question delimited by  ---\n"
                 + "\n"
                 + "---\n"
@@ -102,18 +111,25 @@ public class EmbeddingTestCase {
                 + "---"
                 + "\n Here is a few data to help you:\n"
                 + "";
-        String basePrompt = String.format(completePrompt, prompt);
+        String basePrompt = String.format(completePrompt, question);
         StringBuilder messageBuilder = new StringBuilder(basePrompt);
         for (Content ragContent : ragContents) {
             messageBuilder.append(ragContent.textSegment().text());
         }
-        
-        System.out.println("Answer " + model.generate(systemMessage, UserMessage.from(messageBuilder.toString().substring(0, 4096))).content().text());
-        //        AiServices.builder(CustomerSupportAgent.class)
-        //                .chatLanguageModel(chatModel)
-        //                .contentRetriever(contentRetriever)
-        //                .chatMemory(chatMemory)
-        //                .build();
+
+        ConversationalRetrievalChain chain = ConversationalRetrievalChain.builder()
+                .chatLanguageModel(model)
+                .contentRetriever(contentRetriever)
+                .retrievalAugmentor(DefaultRetrievalAugmentor.builder()
+                        .contentInjector(DefaultContentInjector.builder()
+                                .promptTemplate(PromptTemplate.from(promptTemplate))
+                                .build())
+                        .queryRouter(new DefaultQueryRouter(contentRetriever))
+                        .build())
+                .build();
+        System.out.println("Answer from chain " + chain.execute(question));
+        System.out.println("Answer from model " + model.generate(systemMessage, UserMessage.from(messageBuilder.toString().substring(0, 4096))).content().text());
+
     }
 
 }
